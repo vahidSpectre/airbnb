7e1b22d04dd648bf1341655f66d7518618e8926c
vahidSpectre <vahidsayfollahzadeh@gmail.com>
2024-03-05 04:28:25 +0330

    v1.0

--- src/ui/components/RealEstate.jsx
+++ src/ui/components/RealEstate.jsx
@@ -1,11 +1,97 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { Skeleton } from "@mui/material";
 
-import classes from './RealEstate.module.css'
+import classes from "./RealEstate.module.css";
 
-const RealEstate = ({className}) => {
+const RealEstate = ({ className, imageArray }) => {
+  const [sliderIndex, setSliderIndex] = useState(0);
+  const [touchStar, setTouchStart] = useState(0);
+  const [touchEnd, setTouchEnd] = useState(0);
+  const [isImagesLoading, setIsImagesLoading] = useState(true);
+
+  const buttonVariants = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+  };
+
+  const imagesCount = imageArray.length - 1;
+
+  useEffect(() => {
+    const diff = touchEnd - touchStar;
+    console.log(diff)
+    if (touchEnd !== 0) {
+      if (diff > 0 && sliderIndex > 0) {
+        setSliderIndex((sliderIndex) => (sliderIndex -= 1));
+      }
+      if (diff < 0 && sliderIndex < imagesCount) {
+        setSliderIndex((sliderIndex) => (sliderIndex += 1));
+      }
+    }
+  }, [touchEnd]);
+
   return (
-    <div className={`${classes.wrapper} ${className}`}>RealEstate</div>
-  )
-}
+    <motion.div
+      className={`${classes.wrapper} ${className}`}
+      initial="initial"
+      animate="initial"
+      whileHover={"animate"}
+    >
+      <div
+        className={classes.image_container}
+        style={{ transform: `translateX(${-sliderIndex * 100}%)` }}
+        onTouchStart={(e) => setTouchStart(e.changedTouches[0].clientX)}
+        onTouchEnd={(e) => setTouchEnd(e.changedTouches[0].clientX)}
+      >
+        <Skeleton
+          variant="rectangular"
+          animation="wave"
+          className={`${
+            isImagesLoading ? classes.display_block : classes.display_none
+          }`}
+          sx={{ aspectRatio: 1, paddingTop: "100%" }}
+        />
+        {imageArray.map((img) => {
+          return (
+              <img
+                src={img}
+                key={img}
+                onLoad={() => setIsImagesLoading(false)}
+                className={`${classes.img} ${
+                  isImagesLoading ? classes.display_none : classes.display_block
+                }`}
+              />
+          );
+        })}
+      </div>
+      <motion.button
+        className={classes.next_button}
+        onClick={() => {
+          if (sliderIndex < imagesCount) {
+            setSliderIndex((sliderIndex) => (sliderIndex += 1));
+          }
+        }}
+        variants={buttonVariants}
+        style={{
+          display: `${sliderIndex < imagesCount ? "block" : "none"}`,
+        }}
+      >
+        next
+      </motion.button>
+      <motion.button
+        className={classes.perv_button}
+        onClick={() => {
+          if (sliderIndex > 0) {
+            setSliderIndex((sliderIndex) => (sliderIndex -= 1));
+          }
+        }}
+        variants={buttonVariants}
+        style={{ display: `${sliderIndex > 0 ? "block" : "none"}` }}
+      >
+        perv
+      </motion.button>
+    </motion.div>
+  );
+};
 
-export default RealEstate
+export default RealEstate;
--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -4,17 +4,20 @@ import Section from "../components/Section";
 import classes from "./Home.module.css";
 import RealEstate from "../components/RealEstate";
 
+import data from "../../data/data.json";
+import { nanoid } from "@reduxjs/toolkit";
+
 const Home = () => {
+
   return (
-    <div className={classes.home}>
+    <div className={classes.home} style={{ marginTop: "10rem" }}>
       <Section className={classes.rs_container}>
-        <RealEstate/>
-        <RealEstate/>
-        <RealEstate/>
-        <RealEstate/>
-        <RealEstate/>
-        <RealEstate/>
+        {Object.values(data).map((elem) => {
+          const id = nanoid()
+          return <RealEstate imageArray={elem} key={id}/>;
+        })}
       </Section>
+     
     </div>
   );
 };
